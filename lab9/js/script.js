32a08c1e558e5a849ab5247ce6625c0fdcdcce80
Chris Everhart <chris.everhart@furman.edu>
2026-02-16 14:16:01 -0500

    update for click, display alert, click, update update html page.

--- lab9/js/script.js
+++ lab9/js/script.js
@@ -1,11 +1,20 @@
+var textinput;
+
 function init(){
 //add your javascrip between these two lines of code
- 
+  var button = document.getElementById("entrybutton");
+  textinput = document.getElementById("entryinput");
+  
+  function sendAlert(){    
+    alert("Chris Everhart: " + textinput.value);
+  } 
 
+  function updateTextOutput() {
+    document.getElementById('textoutput').innerHTML = textinput.value;
+  }
 
-
-
-
-
+  button.addEventListener('click', sendAlert);
+  button.addEventListener('click', updateTextOutput);    
+}
 
 window.addEventListener('load', init);
